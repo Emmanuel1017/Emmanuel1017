673ef3e05221b6dcf59fecc5e7ec76f3ee18a696
Emmanuel Korir <koriremmanuel@rocketmail.com>
2026-08-09 03:13:49 +0300

    Add 5xx retry with backoff to recursive_loc (fix transient 502)

--- today.py
+++ today.py
@@ -144,7 +144,13 @@ def recursive_loc(owner, repo_name, data, cache_comment, addition_total=0, delet
         }
     }'''
     variables = {'repo_name': repo_name, 'owner': owner, 'cursor': cursor}
-    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=HEADERS) # I cannot use simple_request(), because I want to save the file before raising Exception
+    # Retry on transient 5xx (GitHub occasionally returns 502/503 during deep history walks)
+    request = None
+    for _attempt in range(6):
+        request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=HEADERS)
+        if request.status_code < 500:
+            break
+        time.sleep(2 * (_attempt + 1))
     if request.status_code == 200:
         if request.json()['data']['repository']['defaultBranchRef'] != None: # Only count commits if repo isn't empty
             return loc_counter_one_repo(owner, repo_name, data, cache_comment, request.json()['data']['repository']['defaultBranchRef']['target']['history'], addition_total, deletion_total, my_commits)
